08234a8f76d5a40d30cc54808322839583ad50a0
YunaGenma <maeyf16@icloud.com>
2026-06-02 06:16:56 +0900

    Add #1: Add user registration feature

---
+++ UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', ['users' => $users]);
+    }
+
+    public function store(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect('/users');
+    }
+}
